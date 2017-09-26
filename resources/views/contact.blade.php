44a44403d5c59695c0fa2384059d93b00cbbfca9
Frank Li <franklee65535@gmail.com>
2017-09-27 11:22:35 +1300

    correct the address

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -13,16 +13,15 @@
                 <h2 class="display-4"></h2>
                 <div class="row">
                     <div class="py-4 px-4 h-100" style="width:50%;">
-                        <h5>Sydney, Australia</h5>
-                        <p>FCS Computer Systems (Australia) Pty Ltd<br>Suite 3, Level 2,<br>88 Pitt Street, Sydney,<br>NSW,
-                            2000, Australia.</p>
+                        <h5>Hamilton, New Zealand</h5>
+                        <p>Itearoa Ltd<br>Suit 10, Level 4,<br>169 London Street, Hamilton<br>
+                            3204, New Zealand.</p>
                     </div>
 
 
                     <div class="py-4 px-4 h-100" style="width:50%;">
-                        <h5>Sydney, Australia</h5>
-                        <p>FCS Computer Systems (Australia) Pty Ltd<br>Suite 3, Level 2,<br>88 Pitt Street, Sydney,<br>NSW,
-                            2000, Australia.</p>
+                        <h5>Beijing, China</h5>
+                        <p>北京市海淀区皂君庙路5号卉园大楼2层</p>
                     </div>
                 </div>
                 <hr class="mt-5" style="border:0.4rem solid #fff;width:16rem;background-color:#fff">
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,7 @@
 @extends('layouts.itearoa')
 
-@section("header")
-    @parent
-@stop
-
 @section('backgroundUrl'){{asset('img/ic.jpeg')}}@stop
-@section('productsfooter')
-    @parent
-@overwrite
 
 @section('sns')
 
-@overwrite
-
-@section('translation')
-    @parent
 @overwrite
